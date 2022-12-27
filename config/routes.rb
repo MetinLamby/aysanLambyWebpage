82839cd02cd53359720452f0f9888543c8249c62
Metin Lamby <metin.lamby@outlook.com>
2022-12-27 20:35:09 +0100

    added old style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+
+  get "videos", to: "pages#videos"
+  get "photos", to: "pages#photos"
+  get "photos/ardaarman", to: "pages#ardaarman"
+  get "photos/brandon", to: "pages#brandon"
+  get "photos/nyc", to: "pages#nyc"
+  get "photos/salomon", to: "pages#salomon"
+
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
